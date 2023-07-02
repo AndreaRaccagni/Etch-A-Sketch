59de875ed32f3bd81e092b94d86e92a8e24cdf51
Andrea <andrearaccagni81@gmail.com>
2023-07-02 23:49:58 +0200

    Add button and hovering to color the grid

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-const gridContainer = document.getElementById('grid-container');
+let gridContainer = document.getElementById('grid-container');
+const button = document.getElementById('start-game');
+let size = 0;
+button.addEventListener('click', () => {
+  do {
+    size = Number(
+      prompt('Choose your grid size.\nSize must be between 10 and 100 ')
+    );
+  } while (size < 10 || size > 100);
 
-const createGrid = (size = 16) => {
+  const para = document.getElementById('grid-size');
+  para.textContent = `Size: ${size} * ${size}`;
+  createGrid(size);
+});
+
+const changeBoxColor = (element) => {
+  element.addEventListener('mouseenter', (e) => {
+    e.target.classList.add('color');
+  });
+};
+
+const createGrid = (size) => {
+  gridContainer.textContent = '';
+  gridContainer.classList.add('new-grid');
   for (let i = 0; i < size; i++) {
     const row = document.createElement('div');
     row.classList = 'row';
@@ -9,14 +30,7 @@ const createGrid = (size = 16) => {
       const cell = document.createElement('div');
       cell.classList = 'cell';
       row.appendChild(cell);
+      changeBoxColor(cell);
     }
   }
 };
-
-createGrid();
-
-// const changeBoxColor = (element) => {
-//   element.addEventListener(onmouseenter, () => {
-//     console.log('hello');
-//   });
-// };
