5ba7fb7d3d6a131b8a436a70d50f6719a5a55b04
Andrea <andrearaccagni81@gmail.com>
2023-07-05 17:13:04 +0200

    Add buttons and rules

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 let gridContainer = document.getElementById('grid-container');
 const button = document.getElementById('start-game');
 let size = 0;
+
 button.addEventListener('click', () => {
   do {
-    size = Number(
-      prompt('Choose your grid size.\nSize must be between 10 and 100 ')
-    );
-  } while (size < 10 || size > 100);
-
-  const para = document.getElementById('grid-size');
-  para.textContent = `Size: ${size} * ${size}`;
+    size = Number(prompt('Enter a size for the grid between 10 and 100 '));
+  } while (isNaN(size) || size < 10 || size > 100);
   createGrid(size);
 });
 
 const changeBoxColor = (element) => {
   element.addEventListener('mouseenter', (e) => {
-    e.target.classList.add('color');
+    if (!e.shiftKey) {
+      e.target.classList.add('color');
+    }
   });
 };
 
-const createGrid = (size) => {
+const createGrid = (size = 16) => {
   gridContainer.textContent = '';
   gridContainer.classList.add('new-grid');
   for (let i = 0; i < size; i++) {
@@ -30,7 +28,15 @@ const createGrid = (size) => {
       const cell = document.createElement('div');
       cell.classList = 'cell';
       row.appendChild(cell);
+      const sizeOutput = document.getElementById('grid-size');
+      sizeOutput.textContent = `Size: ${size} * ${size}`;
       changeBoxColor(cell);
     }
   }
 };
+
+const setRandomColor = () => {
+  return Math.floor(Math.random() * 16777215).toString(16);
+};
+
+createGrid();
